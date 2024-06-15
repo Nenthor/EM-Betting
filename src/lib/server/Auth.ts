5e88b8974c23b287a5efb8a65d3b3f4707a634ed
Nenthor <david@nentwich.dev>
2024-06-15 11:00:00 +0200

    Added refresh token feature

--- src/lib/server/Auth.ts
+++ src/lib/server/Auth.ts
@@ -6,23 +6,42 @@ import { getUserFromCache, updateCacheUser } from './DataHub';
 import { getUser, type User } from './Database';
 
 const SECRET = generateJWTSecret(JWT_SECRET); // We only have one password so we can use it as a secret
-const expiresIn = 60 * 60 * 24 * 30; // 30 days
+const expiresIn = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 export const cookieName = '__session'; // NEEEDS TO BE NAMED LIKE THIS BECAUSE FIREBASE ONLY ALLOWS THIS NAME
+
+interface CookieData {
+	username: string;
+	expiresIn: number;
+}
 
 export async function getUserFromCookies(cookies: Cookies): Promise<User | undefined> {
 	const token = cookies.get(cookieName);
 	if (!token || STOP_AUTH === 'true') return;
 
 	try {
-		const { sub } = jwt.verify(token, SECRET);
+		let { sub } = jwt.verify(token, SECRET);
 		if (typeof sub !== 'string') throw new Error('Invalid token');
 
-		const username = sub.toString();
-		let user = getUserFromCache(username);
+		let cookieData: CookieData;
+		try {
+			cookieData = JSON.parse(sub) as CookieData;
+		} catch (e) {
+			// Replace old token with new one
+			cookieData = { username: sub, expiresIn: Date.now() };
+			console.log('Replaced token of', cookieData.username);
+		}
+
+		if (cookieData.expiresIn < Date.now() + expiresIn * 0.75) {
+			// Refresh token if the expiration date is in less than 75% of the time
+			cookieData.expiresIn = Date.now() + expiresIn;
+			setCookie(cookies, cookieData);
+		}
+
+		let user = getUserFromCache(cookieData.username);
 		if (user) return user; // cached user
 
-		user = await getUser(username);
+		user = await getUser(cookieData.username);
 		if (user) {
 			updateCacheUser(user);
 			return user;
@@ -35,8 +54,8 @@ export async function getUserFromCookies(cookies: Cookies): Promise<User | undef
 
 export function loginUser(user: User, password: string, cookies: Cookies) {
 	if (user && checkPassword(password, user.password)) {
-		const token = jwt.sign({ sub: user.username }, SECRET, { expiresIn });
-		cookies.set(cookieName, token, { path: '/', sameSite: 'strict', secure: true, httpOnly: true, maxAge: expiresIn });
+		const cookieData: CookieData = { username: user.username, expiresIn: Date.now() + expiresIn };
+		setCookie(cookies, cookieData);
 		return true;
 	}
 	return false;
@@ -44,6 +63,11 @@ export function loginUser(user: User, password: string, cookies: Cookies) {
 
 export function logoutUser(cookies: Cookies) {
 	cookies.delete(cookieName, { path: '/' });
+}
+
+function setCookie(cookies: Cookies, cookieData: CookieData) {
+	const token = jwt.sign({ sub: JSON.stringify(cookieData) }, SECRET, { expiresIn: expiresIn / 1000 });
+	cookies.set(cookieName, token, { path: '/', sameSite: 'strict', secure: true, httpOnly: true, expires: new Date(cookieData.expiresIn) });
 }
 
 function generateJWTSecret(secret: string) {
